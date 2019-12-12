a24af2584d3237b6023bff8b203b9112e68d78db
willie_chen <willie_chen@gss.com.tw>
2019-12-12 22:16:09 +0800

    畫面主選單

---
+++ LostCapital/Assets/Scripts/MenuScripts/ExitEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitEvent : MonoBehaviour {
+
+	public void Quit(){
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
